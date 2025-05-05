0f54e7c6463a34f3334a4276977aa4786833ec8a
03Sandeep <168069805+03Sandeep@users.noreply.github.com>
2025-05-06 01:30:53 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "*"],
+    origin: ["https://task-manager-ovck.vercel.app/"],
     credentials: true,
   })
 );
