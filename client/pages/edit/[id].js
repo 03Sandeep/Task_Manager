dd93a709a4cbcff3bab01e4f22eb31e024c58e37
Sandeep Sinha <sandeepsinha1210@gmail.com>
2025-05-06 00:51:01 +0530

    change task from route

--- client/pages/edit/[id].js
+++ client/pages/edit/[id].js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import toast from "react-hot-toast";
-import TaskForm from "../../../components/TaskForm";
+import TaskForm from "../../components/TaskForm.js";
 
 export default function EditTask() {
   const router = useRouter();
